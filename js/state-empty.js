f0192f1d307b86c0ff655ab35639d07e6b38e06d
Stefanos Petrakis <stefanos.petrakis@gmail.com>
2021-10-15 16:30:54 +0300

    Back to using null, should double-check which works better

--- js/state-empty.js
+++ js/state-empty.js
@@ -13,7 +13,7 @@
             $("input[value=0]", e.target).click();
           } else {
             $(e.target).closest('.js-form-item, .js-form-submit, .js-form-wrapper').find('select, input, textarea')
-              .val('')
+              .val(null)
               // For Select2 and other possible listeners.
               .trigger('change');
           }
