29ddf8cb52259a2e8b2a5a3117db622b9f044b06
Stefanos Petrakis <stefanos.petrakis@gmail.com>
2021-10-15 14:58:01 +0300

    Triggering change event after emptying value

--- js/state-empty.js
+++ js/state-empty.js
@@ -8,11 +8,15 @@
     attach: (context) => {
       $(context).on('state:empty', (e) => {
         if (e.trigger && e.value) {
+          debugger;
           if (e.target.type == 'fieldset') {
             $("input[value='']", e.target).click();
             $("input[value=0]", e.target).click();
           } else {
-            $(e.target).closest('.js-form-item, .js-form-submit, .js-form-wrapper').find('select, input, textarea').val('');
+            $(e.target).closest('.js-form-item, .js-form-submit, .js-form-wrapper').find('select, input, textarea')
+              .val(null)
+              // For Select2 and other possible listeners.
+              .trigger('change');
           }
         }
       });
