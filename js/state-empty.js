592273425c8f312becebf3e84dad49659c9fc153
Stefanos Petrakis <stefanos.petrakis+github@gmail.com>
2022-03-30 17:05:46 +0200

    Handling the case of '_none'' for selects

--- js/state-empty.js
+++ js/state-empty.js
@@ -12,8 +12,11 @@
             $("input[value='']", e.target).click();
             $("input[value=0]", e.target).click();
           } else {
+            // Handle the case of the '_none' value for selects.
+            const nullValue = $(e.target).closest('.js-form-item, .js-form-submit, .js-form-wrapper')
+              .find("select option[value='_none']").length > 0 ? '_none' : null;
             $(e.target).closest('.js-form-item, .js-form-submit, .js-form-wrapper').find('select, input, textarea')
-              .val(null)
+              .val(nullValue)
               // For Select2 and other possible listeners.
               .trigger('change');
           }
